14b952aa6374b58eaad41f6103044651c03afed6
hmachefer <hugo.machefer@cpexterne.org>
2017-04-24 00:42:07 +0200

    use scenario file dynamically, by command line

--- main.js
+++ main.js
@@ -10,17 +10,23 @@ let table = [];
 let blocksWorld;
 let lineCounter = 0;
 
-//basic automation that keeps reading each command line and processing
-lineReader.eachLine(stackServices.configuration.SCENARIO_FILE, function(commandLine, last) {
-    let commands;
-    if(last || (commandLine === stackServices.configuration.ORDERS.quit)) {
-        return stackServices.displayTableStacks(table);
-    }
-    if (lineCounter === 0) { // TODO: find a less disgraceful way to get first line
-        blocksWorld = stackServices.init(table, parseInt(commandLine));
-    } else { // lineCounter === 1 or even greater
-        commands = parser.parseCommand(commandLine, blocksWorld);
-        processor.executeCommand(table, commands);
-    }
-    lineCounter++;
-});
+function startReader(scenarioFile) {
+    scenarioFile = scenarioFile || stackServices.configuration.SCENARIO_FILE;
+    //basic automation that keeps reading each command line and processing
+    lineReader.eachLine(scenarioFile, function(commandLine, last) {
+        let commands;
+        if(last || (commandLine === stackServices.configuration.ORDERS.quit)) {
+            return stackServices.displayTableStacks(table);
+        }
+        if (lineCounter === 0) { // TODO: find a less disgraceful way to get first line
+            blocksWorld = stackServices.init(table, parseInt(commandLine));
+        } else { // lineCounter === 1 or even greater
+            commands = parser.parseCommand(commandLine, blocksWorld);
+            processor.executeCommand(table, commands);
+        }
+        lineCounter++;
+    });
+}
+
+const SCENARIO_FILE = process.argv[2];
+startReader(SCENARIO_FILE);
