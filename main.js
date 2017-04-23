1b7239724938095992c3213d931f1e4ffc2a490f
hmachefer <hugo.machefer@cpexterne.org>
2017-04-23 17:50:16 +0200

    remove init(), use "line-reader" module, put instructions in "blocksWorld.txt"

--- main.js
+++ main.js
@@ -1,29 +1,26 @@
 // import { MAX, stack, unStack, displayTableStacks } from 'stackServices';
 const stackServices = require("./stackServices.js");
 const parser = require("./parser.js");
-const process = require("./process.js");
+const processor = require("./process.js");
 const readlineSync = require('readline-sync');
 const chalk = require('chalk');
+const lineReader = require('line-reader');
 
-/**
- * main loop function, basic automation that keeps reading standard input, line by line
- */
-function mainLoop() {
-  let table = [];
-  console.log(chalk.green('Enter the number of blocks'));
-  const n = readlineSync.question("");
-  const blocksWorld = stackServices.init(table, n);
+let table = [];
+let blocksWorld;
+let lineCounter = 0;
 
-  while (true) {
-    commandLine = readlineSync.question(""); // as long as user enters text line..
-    if (commandLine === stackServices.ORDERS.quit) { // in case orders get completed, let's break the infifite loop
-      break;
-    }
-    const commands = parser.parseCommand(commandLine, blocksWorld);
-    process.executeCommand(table, commands);
+//basic automation that keeps reading processing command line
+lineReader.eachLine('blocksWorld.txt', function(commandLine, last) {
+  let commands;
+  if(last || (commandLine === stackServices.ORDERS.quit)) {
+    return stackServices.displayTableStacks(table);
   }
-
-  stackServices.displayTableStacks(table);
-}
-
-mainLoop();
+  if (lineCounter === 0) { // TODO: find a less disgraceful way to get first line
+    blocksWorld = stackServices.init(table, parseInt(commandLine));
+  } else { // lineCounter === 1 or even greater
+    commands = parser.parseCommand(commandLine, blocksWorld);
+    processor.executeCommand(table, commands);
+  }
+  lineCounter++;
+});
